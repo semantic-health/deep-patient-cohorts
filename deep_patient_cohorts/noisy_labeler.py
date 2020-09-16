1e785826b7354680559f014a18968885ee2eb7e8
johngiorgi <johnmgiorgi@gmail.com>
2020-09-16 10:47:23 -0400

    :sparkles: Add negate pregnancy lf

--- deep_patient_cohorts/noisy_labeler.py
+++ deep_patient_cohorts/noisy_labeler.py
@@ -8,6 +8,9 @@ from tqdm import tqdm
 
 from deep_patient_cohorts.utils.common import reformat_icd_code
 
+spacy.prefer_gpu()
+
+
 POSITIVE = 1
 NEGATIVE = -1
 ABSTAIN = 0
@@ -30,36 +33,52 @@ class NoisyLabeler:
             .set_index("ICD9_CODE")
         )
         # If a whitelist is provided, restrict ourselves to the intersection of codes.
-        whitelist = set() if whitelist is None else set(whitelist)
-        self._icd_codes = set(self._descriptions.index) & whitelist
+        self._icd_codes = self._descriptions.index.tolist()
+        if whitelist is not None:
+            self._icd_codes = list(set(self._icd_codes) & set(whitelist))
 
-        self._lfs = [self._exact_match]
+        self._lfs = [self._phrase_match, self._negate_pregancy]
 
-    def _exact_match(self, icd_code: str, documents: List[str]) -> List[int]:
-        # Get the long title, break it up into tokens.
-        # If any of the terms are in document, return POSITIVE, else return ABSTAIN.
-        terms = ",".join(
-            self._descriptions.loc[icd_code, "SHORT_TITLE":"LONG_TITLE"].tolist()
-        ).split(",")
-        matcher = PhraseMatcher(self.nlp.vocab, attr="LEMMA")
-        patterns = list(self.nlp.pipe(terms, disable=["parser", "ner"]))
-        matcher.add(icd_code, None, *patterns)
-        noisy_labels = [
-            POSITIVE if matcher(doc) else ABSTAIN
-            for doc in self.nlp.pipe(documents, disable=["parser", "ner"])
-        ]
-        return noisy_labels
+    def __call__(self, texts: Union[str, List[str], Iterable[str]]) -> Dict[str, np.ndarray]:
 
-    def __call__(self, documents: Union[str, List[str], Iterable[str]]) -> Dict[str, np.ndarray]:
-        if isinstance(documents, str):
-            documents = [documents]
-
+        if isinstance(texts, str):
+            texts = [texts]
         # For each ICD, we need to fill up an array of ( no. of documents X no. of lfs )
         noisy_labels = {icd: [] for icd in self._icd_codes}
         for code, labels in tqdm(noisy_labels.items()):
             for lf in self._lfs:
-                labels.append(lf(code, documents))
+                labels.append(lf(code, texts))
         noisy_labels = {
             icd: np.asarray(labels, dtype=np.int8).T for icd, labels in noisy_labels.items()
         }
+
+        return noisy_labels
+
+    def _phrase_match(self, icd_code: str, texts: List[str]) -> List[int]:
+        # Get the long title, break it up into tokens.
+        # If any of the terms are in document, return POSITIVE, else return ABSTAIN.
+        descriptions = ",".join(
+            self._descriptions.loc[icd_code, "SHORT_TITLE":"LONG_TITLE"].tolist()
+        ).split(",")
+        matcher = PhraseMatcher(self.nlp.tokenizer.vocab, attr="LOWER")
+        patterns = list(self.nlp.tokenizer.pipe(descriptions))
+        matcher.add(icd_code, patterns)
+        docs = self.nlp.tokenizer.pipe(texts)
+        noisy_labels = list(POSITIVE if matcher(doc) else ABSTAIN for doc in docs)
+        return noisy_labels
+
+    def _negate_pregancy(self, icd_code: str, texts: List[str]) -> List[int]:
+        pregnancy_icd_code = self._descriptions[
+            self._descriptions.LONG_TITLE.str.contains("pregnancy", case=False)
+        ].index.tolist()
+        keyword = "sex: m"
+        if icd_code in pregnancy_icd_code:
+            noisy_labels = []
+            for text in texts:
+                if keyword in text:
+                    noisy_labels.append(NEGATIVE)
+                else:
+                    noisy_labels.append(ABSTAIN)
+        else:
+            noisy_labels = [ABSTAIN] * len(texts)
         return noisy_labels
