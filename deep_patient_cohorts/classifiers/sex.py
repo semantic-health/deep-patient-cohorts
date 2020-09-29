f9592733c7f50b762c78594a3471e54dc8eb2914
johngiorgi <johnmgiorgi@gmail.com>
2020-09-29 13:05:34 -0400

    :art: black format the code

--- deep_patient_cohorts/classifiers/sex.py
+++ deep_patient_cohorts/classifiers/sex.py
@@ -7,11 +7,13 @@ from tensorflow.keras.models import load_model
 
 class SexClassifier:
     def __init__(self):
-        self._model = load_model("../data/classifiers/sex/model_autokeras", custom_objects=ak.CUSTOM_OBJECTS)
+        self._model = load_model(
+            "../data/classifiers/sex/model_autokeras", custom_objects=ak.CUSTOM_OBJECTS
+        )
 
     def __call__(self, texts: Union[str, List[str], np.ndarray]) -> str:
         if isinstance(texts, str):
             texts = np.asarray([texts], dtype=str)
         elif isinstance(texts, list):
             texts = np.asarray(texts, dtype=str)
-        return self._model.predict(texts, dtype=str) < 0.5
+        return self._model.predict(texts) < 0.5
