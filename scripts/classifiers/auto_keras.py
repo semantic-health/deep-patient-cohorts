7588683cc7800b11a1bdabd1a08e60c7213d85a3
johngiorgi <johnmgiorgi@gmail.com>
2020-09-29 12:37:13 -0400

    :recycle: Explictly export best model

--- scripts/classifiers/auto_keras.py
+++ scripts/classifiers/auto_keras.py
@@ -14,10 +14,14 @@ np.random.seed(RANDOM_STATE)
 def main(
     input_filepath: str = typer.Argument(..., help="Filepath to the TSV-formatted train dataset."),
     output_directory: str = typer.Argument(
-        "./", help="Directory to save the best model found during the search."
+        "./",
+        help=(
+            "Directory to save the output generated during the search. The best model will be saved"
+            " as 'output_directory/model_autokeras' or 'output_directory/model_autokeras.h5'"
+        ),
     ),
     max_trials: int = typer.Option(
-        100,
+        1000,
         help=(
             "The maximum number of different Keras Models to try."
             " The search may finish before reaching the max_trials."
@@ -28,15 +32,23 @@ def main(
     X = df["text"].values.astype(str)
     y = df["labels"].values
 
-    # Hardcod
-    clf = ak.TextClassifier(max_trials=max_trials, seed=RANDOM_STATE)
-    clf.fit(X, y)
-
     output_directory = Path(output_directory)
     output_directory.mkdir(parents=True, exist_ok=True)
-    clf.export_model(output_directory)
+
+    # Hardcode max_model_size to the upper bound of the first models AutoKeras tries.
+    clf = ak.TextClassifier(max_trials=max_trials, directory=output_directory, seed=RANDOM_STATE)
+    clf.fit(X, y)
+
+    model = clf.export_model()
+    try:
+        output_filepath = output_directory / "model_autokeras"
+        model.save(output_filepath, save_format="tf")
+    except ImportError:
+        output_filepath = output_directory / "model_autokeras.h5"
+        model.save(output_filepath)
     typer.secho(
-        f"Best model saved to {output_directory.absolute()}.", bold=True,
+        f"Best model saved to {output_filepath.absolute()}.",
+        bold=True,
     )
 
 
