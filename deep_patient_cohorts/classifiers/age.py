79163dc9b9833e65d18a92714219dce92e1fb77e
johngiorgi <johnmgiorgi@gmail.com>
2020-10-04 10:35:29 -0400

    :recycle: Classifiers return numpy arrays

--- deep_patient_cohorts/classifiers/age.py
+++ deep_patient_cohorts/classifiers/age.py
@@ -1,14 +1,16 @@
 import re
 from typing import List, Union
 
+import numpy as np
+
 
 class AgeClassifier:
     age_pattern = re.compile(
-        r"(?:\[\**age\s*over\s*)(90)\s*\**]|(\d+)\s*(?:year\s*old|y.\s*o.|yo|year\s*old|year-old|-year-old|-year old)",
+        r"(?:\[\**age\s*over\s*)(90)\s*\**]|(\d+)\s*(?:year\s*old|y.\s*o.|yo|year\s*old|year-old|-year-old|-year old)",  # noqa: E501
         re.IGNORECASE,
     )
 
-    def __call__(self, texts: Union[str, List[str]]) -> Union[int, List[int]]:
+    def __call__(self, texts: Union[str, List[str]]) -> np.ndarray:
         """Returns the extracted ages for each text in `texts` or -1 if no age was found."""
         if isinstance(texts, str):
             texts = [texts]
@@ -17,8 +19,14 @@ class AgeClassifier:
         # The age of the patient tends to be the first age mentioned in the discharge summary.
         # There are two capturing groups, so we need to check which is the non-empty one.
         # -1 denotes a failed attempt to capture an age from the text.
-        ages = [
-            -1 if not match else int(match[0][0]) if match[0][0] else int(match[0][1])
-            for match in matches
-        ]
-        return ages[0] if len(ages) == 1 else ages
+        ages = (
+            np.asarray(
+                [
+                    -1 if not match else int(match[0][0]) if match[0][0] else int(match[0][1])
+                    for match in matches
+                ]
+            )
+            .ravel()
+            .astype(int)
+        )
+        return ages
--- deep_patient_cohorts/classifiers/sex.py
+++ deep_patient_cohorts/classifiers/sex.py
@@ -3,17 +3,20 @@ from typing import List, Union
 import autokeras as ak
 import numpy as np
 from tensorflow.keras.models import load_model
+from pathlib import Path
 
 
 class SexClassifier:
     def __init__(self):
         self._model = load_model(
-            "../data/classifiers/sex/model_autokeras", custom_objects=ak.CUSTOM_OBJECTS
+            Path(__file__).absolute().parents[2] / "data/classifiers/sex/model_autokeras",
+            custom_objects=ak.CUSTOM_OBJECTS,
         )
 
-    def __call__(self, texts: Union[str, List[str], np.ndarray]) -> str:
+    def __call__(self, texts: Union[str, List[str], np.ndarray]) -> np.ndarray:
+        """Returns an array of ints, 0 if the patient is male and 1 if the patient is female."""
         if isinstance(texts, str):
             texts = np.asarray([texts], dtype=str)
         elif isinstance(texts, list):
             texts = np.asarray(texts, dtype=str)
-        return self._model.predict(texts) < 0.5
+        return (self._model.predict(texts) < 0.5).ravel().astype(int)
