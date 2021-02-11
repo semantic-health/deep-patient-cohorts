54e26d69d965b28c98defcd888131d620fe9a220
johngiorgi <johnmgiorgi@gmail.com>
2021-02-11 12:11:19 -0500

    :sparkles: General fit method

--- deep_patient_cohorts/common/utils.py
+++ deep_patient_cohorts/common/utils.py
@@ -1,7 +1,9 @@
 import re
-from typing import List
+from pathlib import Path
+from typing import Optional
 
 import pandas as pd
+from tqdm import tqdm
 
 POSITIVE = 1
 NEGATIVE = -1
@@ -9,305 +11,22 @@ ABSTAIN = 0
 
 IS_A_CID = "116680003"
 
-VASCULAR_SCLEROSIS = [
-    "Segmental glomerulosclerosis",
-    "Arteriolosclerosis",
-    "Phlebosclerosis",
-    "Venofibrosis",
-    "Venosclerosis",
-    "Sclerosing segmental glomerulosclerosis with hyalinosis",
-    "Elastic vascular sclerosis",
-    "Atherosclerotic fibrous plaque",
-    "Arteriosclerosis",
-    "Calcified atheromatous plaque",
-    "Arteriolosclerosis with fibrinoid necrosis",
-    "Vascular wall degeneration",
-    "Atherosclerosis",
-    "Atheromatosis",
-    "Nodular glomerulosclerosis",
-    "Cystic medial necrosis",
-    "Cystic medionecrosis",
-    "Adventitial degeneration",
-    "Atheroma",
-    "Atheromatous plaque",
-    "Atheromatous degeneration",
-    "Fatty streaks",
-    "Lipid streaks",
-    "Yellow streaks",
-    "Yellow dots",
-    "Fatty dots",
-    "Ulcerated atheromatous plaque",
-    "Fibroelastosis",
-    "Complicated atheromatous plaque",
-    "Intercapillary glomerulosclerosis",
-    "Glomerulosclerosis",
-    "Medial degeneration",
-    "Medial calcification",
-    "Angiolithic degeneration",
-    "Calcified atheroma",
-    "Ulcerated atheroma",
-    "Complicated atheroma",
-    "Glomerulosclerosis (M-53300)",
-]
-
-# The textual descriptions of every child of the Snowmed Concept "Heart Diseases" (56265001)
-HEART_DISEASES = [
-    "Acute heart disease",
-    "Heart valve disorder",
-    "Valvular heart disease",
-    "Heart valve disease",
-    "Myxedema heart disease",
-    "Myxoedema heart disease",
-    "Hereditary coproporphyria",
-    "Berger-Goldberg syndrome",
-    "CPO deficiency",
-    "CPRO deficiency",
-    "Porphyria hepatica II",
-    "Watson syndrome",
-    "Cardiomegaly",
-    "Cardiac hypertrophy",
-    "Cor bovinum",
-    "Coronary artery disease",
-    "Cyanotic congenital heart disease",
-    "Congenital heart disease",
-    "Congenital anomaly of heart",
-    "Senile cardiac amyloidosis",
-    "AS transthyretin amyloidosis",
-    "Cardiac amyloidosis",
-    "Abnormal fetal heart beat first noted during labor AND/OR delivery in liveborn infant",
-    "Abnormal fetal heart beat first noted during labor or delivery in liveborn infant",
-    "Rheumatic heart disease",
-    "Rheumatic carditis",
-    "Rheumatic pancarditis",
-    "Ventricular tachycardia",
-    "Cardiac complication",
-    "Mechanical complication due to heart valve prosthesis",
-    "Conduction disorder of the heart",
-    "Mulberry heart disease",
-    "Disease of pericardium",
-    "Pericardial disorder",
-    "Myocardial disease",
-    "Thyrotoxic heart disease",
-    "Cardiopulmonary schistosomiasis",
-    "Pancarditis",
-    "Hypertensive heart disease",
-    "Hypertensive cardiopathy",
-    "Hypertensive cardiomegaly",
-    "Hypertensive cardiovascular disease",
-    "Failed attempted abortion with cardiac arrest AND/OR failure",
-    "Failed attempted abortion with cardiac arrest or failure",
-    "Cardiac sarcoidosis",
-    "Heart disease in mother complicating pregnancy, childbirth AND/OR puerperium",
-    "Heart disease, in mother complicating pregnancy, childbirth or puerperium",
-    "Neoplasm of heart",
-    "Neoplasm of mesothelial tissue of pericardium",
-    "Cor pulmonale",
-    "Acardiac monster",
-    "Acardius",
-    "Acardiacus",
-    "Acardiac twins",
-    "Injury of heart",
-    "Cardiac edema",
-    "Sudden cardiac death",
-    "Functional cardiac disorder",
-    "Endocardial disease",
-    "Healed bacterial endocarditis",
-    "Disorder of cardiac function",
-    "Endocardiopathy",
-    "Neoplasm of heart AND/OR pericardium",
-    "Chronic heart disorder",
-    "Chronic disorder of heart",
-    "Chronic disease of heart",
-    "Chronic heart disease",
-    "Infectious disease of heart",
-    "Structural disorder of heart",
-    "Biopsy of lesion of wall of heart",
-    "Malignant hypertensive heart disease",
-    "Angina",
-    "Cardiac angina",
-    "Angina pectoris",
-    "Stenocardia",
-    "Anginal syndrome",
-    "AP - Angina pectoris",
-    "Ischemic heart disease - angina",
-    "Ischaemic heart disease - angina",
-    "Certain current complications following acute myocardial infarction",
-    "Other forms of heart disease",
-    "Sudden cardiac death, so described",
-    "Cardiac septal defect, acquired",
-    "[X]Other heart disorders in other diseases classified elsewhere",
-    "Cardiac arrest following abortive pregnancy",
-    "Cardiac failure following abortive pregnancy",
-    "Obstetric anaesthesia with cardiac complications",
-    "Obstetric anesthesia with cardiac complications",
-    "Cardiac complications of anesthesia during labor and delivery",
-    "Cardiac complications of anaesthesia during labour and delivery",
-    "Eisenmenger's complex",
-    "Atresia of cardiac vein",
-    "Hemicardia",
-    "Anomalous bands of heart",
-    "Injury to heart and lung",
-    "Sympathotonic orthostatic hypotension",
-    "Athlete's heart",
-    "Athletic heart syndrome",
-    "Cardiac transplant disorder",
-    "Type 2 dissection of thoracic aorta",
-    "Aortic root dissection",
-    "Type II dissection of thoracic aorta",
-    "Leakage due to cardiac device",
-    "Disorder of implantable defibrillator",
-    "ICD - Disorder of implantable cardiac defibrillator",
-    "Disorder of implantable cardiovertor",
-    "Heart disease during pregnancy",
-    "Mitral sub-valve apparatus fibrosis",
-    "Mitral sub-valve apparatus calcification",
-    "Aortic root congenital abnormality",
-    "Right atrial abnormality",
-    "RA - Right atrial abnormality",
-    "Left atrial abnormality",
-    "LA - Left atrial abnormality",
-    "Right ventricle abnormality",
-    "RV - Right ventricular abnormality",
-    "Right ventricular abnormality",
-    "SOV - Sinus of Valsalva abnormality",
-    "Sinus of Valsalva abnormality",
-    "Cardiac complications of care",
-    "Disorder of cardiac pacemaker system",
-    "Pulmonary heart disease",
-    "Cardiac disease in pregnancy",
-    "Cardiac failure developing in the perinatal period",
-    "Neonatal cardiac failure",
-    "Acute/subacute carditis",
-    "Enlarge heart septal foramen",
-    "Cardiac glycogen phosphorylase kinase deficiency",
-    "Tuberculosis of heart",
-    "Tumour of heart",
-    "Tumor of heart",
-    "CHD - Congenital heart disease",
-    "Amyloid heart muscle disease",
-    "Abnormal fetal heart beat first noted during labour AND/OR delivery in liveborn infant",
-    "Cardiac complication of procedure",
-    "Disorder of heart valve",
-    "HHD - Hypertensive heart disease",
-    "HCP - Hereditary coproporphyria",
-    "Coproporphyrinogen oxidase deficiency",
-    "Watson's syndrome",
-    "CPO - Coproporphyrinogen oxidase deficiency",
-    "Cardiac oedema",
-    "VT - Ventricular tachycardia",
-    "Cardiac arrhythmia",
-    "Cardiac dysrhythmia",
-    "Arrhythmia",
-    "Cardiac dysrhythmias",
-    "Cardiac arrhythmias",
-    "Disorder of heart rhythm",
-    "Disorder of heart conduction",
-    "Disorder of pericardium",
-    "Disorder of myocardium",
-    "Disorder of heart muscle",
-    "Complication of transplanted heart",
-    "Carditis",
-    "Abscess of cardiac septum",
-    "Ventricular tachyarrhythmia",
-    "Disease of the coronary arteries",
-    "Disorder of coronary artery",
-    "Ischemic heart disease",
-    "Ischaemic heart disease",
-    "IHD - Ischemic heart disease",
-    "IHD - Ischaemic heart disease",
-    "Disorder of cardiac ventricle",
-    "Sinistrocardia",
-    "Left sided heart",
-    "Heart predominantly in left hemithorax",
-    "Disorder of transplanted heart",
-    "Fetal heart disorder",
-    "Heart disease due to ionizing radiation",
-    "Enlarged septal foramen of heart",
-    "Heart disease due to ionising radiation",
-    "Heart disease due to radiation",
-    "Abnormal foetal heart beat first noted during labour AND/OR delivery in liveborn infant",
-    "Eisenmenger complex",
-    "Outflow tract abnormality in solitary indeterminate ventricle",
-    "Congenital abnormality of middle cardiac vein",
-    "Enlarged heart",
-    "Cardiac abnormality due to heart abscess",
-    "Disorder of left atrium",
-    "Disorder of right atrium",
-    "Right heart failure due to disorder of lung",
-    "Right heart failure due to pulmonary disease",
-    "Termination of pregnancy complicated by cardiac arrest and/or failure",
-    "Induced termination of pregnancy complicated by cardiac arrest and/or failure",
-    "Failed attempted termination of pregnancy with cardiac arrest and/or failure",
-    "Foetal heart disorder",
-    "Cardiac disorder due to typhoid fever",
-    "Heart disease co-occurrent with human immunodeficiency virus infection",
-    "Heart disease caused by ionizing radiation",
-    "Heart disease caused by ionising radiation",
-    "Heart disease caused by radiation",
-    "Cardiac complication of anesthesia during the puerperium",
-    "Cardiac complication of anaesthesia during the puerperium",
-    "Acquired cardiac septal defect",
-    "Make heart septal defect",
-    "Dissection of ascending aorta",
-    "Disorder of atrium following procedure",
-    "Postprocedural atrial complication",
-    "Stiff heart syndrome",
-    "Wild-type transthyretin cardiac amyloidosis",
-    "Disorder of cardiac atrium",
-    "Atrial cardiopathy",
-]
-
-# The textual descriptions of every child of the Snowmed Concept "Cardiovascular Agents" (373247007)
-CARDIAC_DRUGS = [
-    "Capillary active drug",
-    "Capillary protectant",
-    "Vasoprotectant",
-    "3-Methoxy-o-desmethylencainide",
-    "Pirmenol",
-    "Saluretic - chemical",
-    "Capillary active agent",
-    "Antiplatelet agent",
-    "Hypotensive agent",
-    "Diuretic",
-    "Thrombolytic agent",
-    "Antiarrhythmic agent",
-    "Vasoconstrictor",
-    "Inotropic agent",
-    "Antivaricose agent",
-    "Cardiac agent",
-    "beta-Blocking agent",
-    "Cardiac adrenergic blocking agent",
-    "Antilipemic agent",
-    "Alpha-adrenergic blocking agent",
-    "Calcium channel blocker",
-    "Antilipaemic agent",
-    "Beta-adrenoceptor blocking agent",
-    "Cardiac non-specific adrenergic blocking agent",
-    "Lipid lowering agent",
-    "Alpha-adrenoceptor blocking agent",
-    "Capillary-active drug",
-    "Cardioplegic solution agent",
-    "Antihypertensive agent",
-    "Ivabradine",
-    "Ivabradine hydrochloride",
-    "Adenosine A2 receptor agonist",
-    "Adenosine A2 agonist",
-    "Alpha adrenergic receptor antagonist",
-    "Beta adrenergic receptor antagonist",
-    "Substance with adenosine A2 receptor agonist mechanism of action",
-    "Substance with alpha adrenergic receptor antagonist mechanism of action",
-    "Substance with beta adrenergic receptor antagonist mechanism of action",
-    "Substance with calcium channel blocker mechanism of action",
-    "3-methoxy-o-desmethylencainide",
-    "Modecainide",
-    "Vasopressor",
-]
+TERM_LISTS = Path(__file__).parent / "term_lists"
+# The textual descriptions of every child of the Snowmed Concept "Heart disease" (56265001)
+HEART_DISEASES: set = set((TERM_LISTS / "heart_disease.txt").read_text().split("\n"))
+# The textual descriptions of every child of the Snowmed Concept "Cardiovascular agents" (373247007)
+CARDIAC_DRUGS: set = set((TERM_LISTS / "cardiac_drugs.txt").read_text().split("\n"))
+# The textual descriptions of every child of the Snowmed Concept "Vascular sclerosis" (107671003)
+VASCULAR_SCLEROSIS: set = set((TERM_LISTS / "vascular_sclerosis.txt").read_text().split("\n"))
+# The textual descriptions of every child of the Snowmed Concept "Cardiovascular investigation" (276341003)
+CARDIAC_PROCEDURES: set = set((TERM_LISTS / "cardiac_procedures.txt").read_text().split("\n"))
 
 
 def reformat_icd_code(icd_code: str, is_diag: bool = True) -> str:
-    """Put a period in the right place because the MIMIC-3 data files exclude them.
+    """Put a period in the right place because the MIMIC-III data files exclude them.
     Generally, procedure ICD codes have dots after the first two digits, while diagnosis
     ICD codes have dots after the first three digits.
+
     Adopted from: https://github.com/jamesmullenbach/caml-mimic
     """
     icd_code = "".join(icd_code.split("."))
@@ -323,27 +42,49 @@ def reformat_icd_code(icd_code: str, is_diag: bool = True) -> str:
     return icd_code
 
 
-def get_snowmed_children_descriptions(
-    relationships_filepath: str, description_filepath: str, destination_id: str
-) -> List[str]:
-    """Given a parent concept in Snowmed (destination_id), returns the textual description of all
-    its children concepts (i.e. those that have a "is a" relationship with the parent concept).
-    Requires the filepaths to the relationship (relationships_filepath) and
-    description (description_filepath) files from a Snowmed release.
+def create_term_list(
+    destination_id: str,
+    description_filepath: str,
+    relationships_filepath: str,
+    output_filepath: str,
+    num_levels: Optional[int] = None,
+    max_length: Optional[int] = None,
+) -> None:
+    """Given a parent concept in Snowmed (`destination_id`), creates a text file at
+    `output_filepath` containing the textual descriptions of all its children concepts, i.e. those
+    that have a "is a" relationship with the parent concept. Requires the filepaths to the
+    relationship (`relationships_filepath`) and description (`description_filepath`) files from a
+    Snowmed release.
+
+    # Parameters:
+
+    destination_id : `str`, required.
+        The Snowmed ID of the parent concept.
+    description_filepath : `str`, required.
+        The filepath to the decription file from a Snowmed release.
+    relationships_filepath : `str`, required.
+        The filepath to the relationships file from a Snowmed release.
+    output_filepath : `str`, required.
+        The filepath to save the retrieved term list.
+    num_levels : `int`, optional.
+        The number of levels to traverse in the Snowmed tree when retrieving child concepts.
+        A sensible default is 2.
+    max_length : `int`, optional.
+        If not `None`, only terms with a number of whitespace tokens equal to or less than this
+        number are retained. A sensible default is 4.
     """
-    relationships = pd.read_csv(
-        relationships_filepath,
-        sep="\t",
-        usecols=["sourceId", "destinationId", "relationshipGroup", "typeId"],
-        dtype={"sourceId": str, "destinationId": str, "relationshipGroup": str, "typeId": str},
-    )
     descriptions = pd.read_csv(
         description_filepath,
         sep="\t",
         usecols=["conceptId", "term"],
         dtype={"conceptId": str, "term": str},
     )
-
+    relationships = pd.read_csv(
+        relationships_filepath,
+        sep="\t",
+        usecols=["sourceId", "destinationId", "typeId"],
+        dtype={"sourceId": str, "destinationId": str, "typeId": str},
+    )
     children_ids = (
         relationships[
             (relationships["destinationId"] == destination_id)
@@ -352,14 +93,51 @@ def get_snowmed_children_descriptions(
         .unique()
         .tolist()
     )
-    children_descriptions = descriptions[descriptions["conceptId"].isin(children_ids)]["term"]
+    previous_children_ids = children_ids[:]
+
+    current_level = 0
+    while True:
+        current_children_ids = []
+        for child_id in tqdm(previous_children_ids):
+            current_children_ids.extend(
+                relationships[
+                    (relationships["destinationId"] == child_id)
+                    & (relationships["typeId"] == IS_A_CID)
+                ]["sourceId"]
+                .unique()
+                .tolist()
+            )
+        current_level += 1
+        previous_children_ids = current_children_ids[:]
+        children_ids.extend(current_children_ids)
+        if not previous_children_ids or (num_levels and current_level == num_levels):
+            break
 
     def _postprocess(text: str) -> str:
+        # Remove irrelevant text in textual description. Unfortunately there doesn't
+        # appear to be a pattern, so we have to target these one-by-one.
         text = re.sub(
-            r"(\(substance\)|\(disorder\)|\(morphologic abnormality\)|,?\s*NOS)", "", text
+            r"\((finding|substance|disorder|morphologic\sabnormality|(medicinal\s)?product|\w-\d+|"
+            r"procedure|regime/therapy)\)|-\s*chemical|-class of antibiotic-|\[Ambiguous\]|,?\s*NOS",
+            "",
+            text,
         )
-        return text.strip()
+        # Remove whitespace, newlines and tabs.
+        text = " ".join(text.strip().split())
+        return text
 
-    children_descriptions = children_descriptions.apply(_postprocess)
+    children_descriptions = (
+        # Include the description of the parent concept.
+        descriptions[descriptions["conceptId"].isin(set(children_ids + [destination_id]))]["term"]
+        .apply(_postprocess)
+        .unique()
+        .tolist()
+    )
+    if max_length:
+        children_descriptions = [
+            descr for descr in children_descriptions if len(descr.split()) <= max_length
+        ]
 
-    return children_descriptions.unique().tolist()
+    output_filepath: Path = Path(output_filepath)
+    output_filepath.parents[0].mkdir(parents=True, exist_ok=True)
+    output_filepath.write_text("\n".join(children_descriptions))
